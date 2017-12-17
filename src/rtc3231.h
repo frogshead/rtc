56d078bc94e21413059af7136df5bc5d4009ace0
Mikko Viitamäki <mikko.viitamaki@gmail.com>
2017-12-17 14:27:54 +0200

    backup to github :)

--- src/rtc3231.h
+++ src/rtc3231.h
@@ -14,61 +14,48 @@
 #ifndef __DS3231_H__
 #define __DS3231_H__
 
-#include <stdio.h>
+#include <stdint.h>
+#include "dst.h"
+
 
 #define RTC_WADDR 0xd0
 #define RTC_RADDR 0xd1
 
-struct rtc_time
-{
-	uint8_t sec;
-	uint8_t min;
-	uint8_t hour;
-};
-
-struct rtc_date
-{
-	uint8_t wday;
-	uint8_t day;
-	uint8_t month;
-	uint8_t year;
-};
 
 /*
  * Clock initialization
  */
-void rtc3231_init(void);
+ void rtc3231_init(void);
 
-/**
- * Reading time from clock
- * @time: time structure with reading data
- */
-void rtc3231_read_time(struct rtc_time *time);
+// /**
+//  * Reading time from clock
+//  * @time: time structure with reading data
+//  */
 
-/**
- * Reading date from clock
- * @date: date structure with reading data
- */
-void rtc3231_read_date(struct rtc_date *date);
+void rtc3231_read_time(struct rtc_tm *time);
+
+// /**
+//  * Reading date from clock
+//  * @date: date structure with reading data
+//  */
+// void rtc3231_read_date(struct rtc_date *date);
 
 /**
  * Reading date and time from clock
- * @time: time structure with reading data
- * @date: date structure with reading data
+ * @time datetime structure
  */
-void rtc3231_read_datetime(struct rtc_time *time, struct rtc_date *date);
+void rtc3231_read_datetime(struct rtc_tm *time);
+// /**
+//  * Writing new time in clock
+//  * @time: time structure for writing data
+//  */
+// void rtc3231_write_time(struct rtc_tm *time);
 
-/**
- * Writing new time in clock
- * @time: time structure for writing data
- */
-void rtc3231_write_time(struct rtc_time *time);
-
-/**
- * Writing new date in clock
- * @date: date structure for writing data
- */
-void rtc3231_write_date(struct rtc_date *date);
+// /**
+//  * Writing new date in clock
+//  * @date: date structure for writing data
+//  */
+// void rtc3231_write_date(struct rtc_date *date);
 
 
 #endif
